0c2ee79f7dc7b9069f53b57c7bf553a40568ca4d
Brian Sokas <brian@ethermed.ai>
2020-02-24 20:58:09 -0500

    Add small table formatting and prep rendering function and logic for the fetching of names

--- src/components/NameTable.jsx
+++ src/components/NameTable.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import '../App.css';
 
-export default class NameTable extends React.Component {
+export interface NameList {
+  Name: String,
+  Surname: String,
+  Gender: String,
+  Region: String,
+  Email?: String,
+  Phone?: String,
+}
+
+interface State {
+  nameList: Name[],
+}
+
+export default class NameTable extends React.Component<State> {
+
+  constructor(){
+    super();
+    this.state = { nameList: undefined };
+  }
+
+  renderNamesDynamically(){
+    //Work in progress, need to import Promise behavior
+  }
 
   renderNames(){
+    //For now this is test data, I didn't have time to make it dynamically
+    //fetched from the server
     return (
       <>
       <tr>
@@ -11,18 +35,24 @@ export default class NameTable extends React.Component {
         <td>Smith</td>
         <td>Male</td>
         <td>USA</td>
+        <td>jsmith@yahoo.com</td>
+        <td>303-328-8654</td>
       </tr>
       <tr>
         <td>Jane</td>
         <td>Smith</td>
         <td>Female</td>
         <td>USA</td>
+        <td>jsmith@hotmail.com</td>
+        <td>777-867-5309</td>
       </tr>
       </>
     )
   }
 
   render(){
+    const { nameList } = this.state;
+
     return (
       <div className="name-table">
         <table>
@@ -31,8 +61,10 @@ export default class NameTable extends React.Component {
             <th>Last Name</th>
             <th>Gender</th>
             <th>Region</th>
+            <th>Email</th>
+            <th>Phone Number</th>
           </tr>
-          {this.renderNames()}
+          {nameList ? this.renderNamesDynamically() : this.renderNames()}
         </table>
       </div>
     )
